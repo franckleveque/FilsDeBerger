d8ddfb6294e345ea6446d356aa818371d7e77c3a
Franck LEVEQUE <franck.leveque2@free.fr>
2010-05-08 22:55:18 +0200

    Fixed some stylecop issues

--- src/FilsDeBerger/Main.cs
+++ src/FilsDeBerger/Main.cs
@@ -1,60 +1,41 @@
-#region LICENSE
-/*
- * Copyright (C) 2005 Rob Loach (http://www.robloach.net)
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- * 
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- * 
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- */
-#endregion LICENSE
-
-using System;
-using System.IO;
-using System.Drawing;
-
-using SdlDotNet;
-using SdlDotNet.Graphics;
-using SdlDotNet.Graphics.Sprites;
-using SdlDotNet.Core;
-using SdlDotNet.Input;
 using FilsDeBerger.SDL;
 
-namespace SdlDotNetExamples.SmallDemos
+namespace FilsDeBerger
 {
+    using System;
+    using System.Drawing;
+    using System.IO;
+
+    using SdlDotNet;
+    using SdlDotNet.Core;
+    using SdlDotNet.Graphics;
+    using SdlDotNet.Graphics.Sprites;
+    using SdlDotNet.Input;
+
+    /// <summary>
+    /// Main class of the game
+    /// </summary>
     public class FilsDeBerger : IDisposable
     {
-        // List of characters to move in the world
-        private Character[] characters; 
+        #region Fields
 
-        [STAThread]
-        public static void Main()
-        {
-            // Start the application
-            FilsDeBerger app = new FilsDeBerger();
-            app.Go();
-        }
+        /// <summary>
+        /// List of characters to move in the world
+        /// </summary>
+        private Character[] characters;
 
-        public void Go()
-        {
-            // Setup the SDL.NET events
-            Events.Fps = 50;
-            Events.Tick += new EventHandler<TickEventArgs>(Events_Tick);
-            Events.Quit += new EventHandler<QuitEventArgs>(Events_Quit);
-            Events.KeyboardDown += new EventHandler<KeyboardEventArgs>(Events_KeyboardDown);
-            Events.KeyboardUp += new EventHandler<KeyboardEventArgs>(Events_KeyboardUp);
-            Events.Run();
-        }
+        /// <summary>
+        /// Indicates whether object have been disposed or not
+        /// </summary>
+        private bool disposed;
 
+        #endregion Fields
+
+        #region Constructors
+
+        /// <summary>
+        /// Initializes a new instance of the FilsDeBerger class.
+        /// </summary>
         public FilsDeBerger()
         {
             // Start up the window
@@ -63,48 +44,226 @@ namespace SdlDotNetExamples.SmallDemos
             Video.SetVideoMode(800, 600);
 
             Character.Path = @"Graphics\Charset";
-            characters = new Character[2];
-            characters[0] = new Character("shepherd.png");
-            characters[0].Speed = 2;
-            characters[1] = new Character("sheep.png");
-            characters[1].Speed = 1;
-            foreach (Character hero in characters)
+            this.characters = new Character[2];
+            this.characters[0] = new Character("shepherd.png");
+            this.characters[0].Speed = 2;
+            this.characters[1] = new Character("sheep.png");
+            this.characters[1].Speed = 1;
+            foreach (Character hero in this.characters)
             {
                 hero.Animate = false;
             }
 
             // Put him in the center of the screen
-            characters[0].Center = new Point(
+            this.characters[0].Center = new Point(
                 Video.Screen.Width / 2,
                 Video.Screen.Height / 2);
 
-            characters[1].Center = new Point(
-                Video.Screen.Width / 2 + 50,
+            this.characters[1].Center = new Point(
+                (Video.Screen.Width / 2) + 50,
                 Video.Screen.Height / 2);
         }
 
+        /// <summary>
+        /// Finalizes an instance of the FilsDeBerger class.
+        /// </summary>
+        ~FilsDeBerger()
+        {
+            this.Dispose(false);
+        }
+
+        #endregion Constructors
+
+        #region Methods
+
+        /// <summary>
+        /// Main sub of software
+        /// </summary>
+        [STAThread]
+        public static void Main()
+        {
+            // Start the application
+            FilsDeBerger app = new FilsDeBerger();
+            app.Go();
+        }
+
+        /// <summary>
+        /// End class life
+        /// </summary>
+        public void Close()
+        {
+            this.Dispose();
+        }
+
+        /// <summary>
+        /// Execute task defined by the software linked to freeing or redefining of unmanaged ressources
+        /// </summary>
+        public void Dispose()
+        {
+            this.Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        /// <summary>
+        /// Launch the SDL Events loop
+        /// </summary>
+        public void Go()
+        {
+            // Setup the SDL.NET events
+            Events.Fps = 50;
+            Events.Tick += new EventHandler<TickEventArgs>(this.Events_Tick);
+            Events.Quit += new EventHandler<QuitEventArgs>(this.Events_Quit);
+            Events.KeyboardDown += new EventHandler<KeyboardEventArgs>(this.Events_KeyboardDown);
+            Events.KeyboardUp += new EventHandler<KeyboardEventArgs>(this.Events_KeyboardUp);
+            Events.Run();
+        }
+
+        /// <summary>
+        /// Execute task defined by the software linked to freeing or redefining of unmanaged ressources
+        /// </summary>
+        /// <param name="disposing">Define if dispose of linked objects should occurs</param>
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    for (int i = 0; i < this.characters.GetLength(0); i++)
+                    {
+                        if (this.characters[i] != null)
+                        {
+                            this.characters[i].Dispose();
+                            this.characters[i] = null;
+                        }
+                    }
+                }
+
+                this.disposed = true;
+            }
+        }
+
+        /// <summary>
+        /// Event handler for the KeyboardDown event
+        /// </summary>
+        /// <param name="sender">Sender of the event</param>
+        /// <param name="e">Parameters of the event</param>
+        private void Events_KeyboardDown(object sender, KeyboardEventArgs e)
+        {
+            // Check which key was pressed and change the animation accordingly
+            switch (e.Key)
+            {
+                case Key.LeftArrow:
+                    foreach (Character hero in this.characters)
+                    {
+                        hero.CurrentAnimation = "WalkLeft";
+                        hero.Animate = true;
+                    }
+
+                    break;
+                case Key.RightArrow:
+                    foreach (Character hero in this.characters)
+                    {
+                        hero.CurrentAnimation = "WalkRight";
+                        hero.Animate = true;
+                    }
+
+                    break;
+                case Key.DownArrow:
+                    foreach (Character hero in this.characters)
+                    {
+                        hero.CurrentAnimation = "WalkDown";
+                        hero.Animate = true;
+                    }
+
+                    break;
+                case Key.UpArrow:
+                    foreach (Character hero in this.characters)
+                    {
+                        hero.CurrentAnimation = "WalkUp";
+                        hero.Animate = true;
+                    }
+
+                    break;
+                case Key.Escape:
+                case Key.Q:
+                    Events.QuitApplication();
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// Event handler for the KeyboardUp event
+        /// </summary>
+        /// <param name="sender">Sender of the event</param>
+        /// <param name="e">Parameters of the event</param>
+        private void Events_KeyboardUp(object sender, KeyboardEventArgs e)
+        {
+            // Check which key was brought up and stop the hero if needed
+            if (e.Key == Key.LeftArrow && this.characters[0].CurrentAnimation == "WalkLeft")
+            {
+                foreach (Character hero in this.characters)
+                {
+                    hero.Animate = false;
+                }
+            }
+            else if (e.Key == Key.UpArrow && this.characters[0].CurrentAnimation == "WalkUp")
+            {
+                foreach (Character hero in this.characters)
+                {
+                    hero.Animate = false;
+                }
+            }
+            else if (e.Key == Key.DownArrow && this.characters[0].CurrentAnimation == "WalkDown")
+            {
+                foreach (Character hero in this.characters)
+                {
+                    hero.Animate = false;
+                }
+            }
+            else if (e.Key == Key.RightArrow && this.characters[0].CurrentAnimation == "WalkRight")
+            {
+                foreach (Character hero in this.characters)
+                {
+                    hero.Animate = false;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Event handler for the Quit event
+        /// </summary>
+        /// <param name="sender">Sender of the event</param>
+        /// <param name="e">Parameters of the event</param>
+        private void Events_Quit(object sender, QuitEventArgs e)
+        {
+            Events.QuitApplication();
+        }
+
+        /// <summary>
+        /// Event handler for tick events
+        /// </summary>
+        /// <param name="sender">Sender of the event</param>
+        /// <param name="e">Parameters of the event</param>
         private void Events_Tick(object sender, TickEventArgs e)
         {
-
             // Clear the screen, draw the hero and output to the window
             Video.Screen.Fill(Color.DarkGreen);
             try
             {
-                foreach (Character hero in characters)
+                foreach (Character hero in this.characters)
                 {
                     Video.Screen.Blit(hero);
                 }
-                //hero.Render(Video.Screen);
             }
             catch (System.ArgumentOutOfRangeException ex)
             {
                 Console.WriteLine(ex.StackTrace.ToString());
             }
+
             Video.Screen.Update();
 
-
             // If the hero is animated, he is walking, so move him around!
-            foreach (Character hero in characters)
+            foreach (Character hero in this.characters)
             {
                 if (hero.Animate)
                 {
@@ -126,153 +285,8 @@ namespace SdlDotNetExamples.SmallDemos
                     }
                 }
             }
-
-
         }
 
-        private void Events_KeyboardDown(object sender, KeyboardEventArgs e)
-        {
-            // Check which key was pressed and change the animation accordingly
-            switch (e.Key)
-            {
-                case Key.LeftArrow:
-                    foreach (Character hero in characters)
-                    {
-                        hero.CurrentAnimation = "WalkLeft";
-                        hero.Animate = true;
-                    }
-
-                    break;
-                case Key.RightArrow:
-                    foreach (Character hero in characters)
-                    {
-                        hero.CurrentAnimation = "WalkRight";
-                        hero.Animate = true;
-                    }
-
-                    break;
-                case Key.DownArrow:
-                    foreach (Character hero in characters)
-                    {
-                        hero.CurrentAnimation = "WalkDown";
-                        hero.Animate = true;
-                    }
-
-                    break;
-                case Key.UpArrow:
-                    foreach (Character hero in characters)
-                    {
-                        hero.CurrentAnimation = "WalkUp";
-                        hero.Animate = true;
-                    }
-
-                    break;
-                case Key.Escape:
-                case Key.Q:
-                    Events.QuitApplication();
-                    break;
-            }
-        }
-
-        private void Events_KeyboardUp(object sender, KeyboardEventArgs e)
-        {
-            // Check which key was brought up and stop the hero if needed
-            if (e.Key == Key.LeftArrow && characters[0].CurrentAnimation == "WalkLeft")
-            {
-                foreach (Character hero in characters)
-                {
-                    hero.Animate = false;
-                }
-            }
-            else if (e.Key == Key.UpArrow && characters[0].CurrentAnimation == "WalkUp")
-            {
-                foreach (Character hero in characters)
-                {
-                    hero.Animate = false;
-                }
-            }
-            else if (e.Key == Key.DownArrow && characters[0].CurrentAnimation == "WalkDown")
-            {
-                foreach (Character hero in characters)
-                {
-                    hero.Animate = false;
-                }
-            }
-            else if (e.Key == Key.RightArrow && characters[0].CurrentAnimation == "WalkRight")
-            {
-                foreach (Character hero in characters)
-                {
-                    hero.Animate = false;
-                }
-            }
-        }
-        private void Events_Quit(object sender, QuitEventArgs e)
-        {
-            Events.QuitApplication();
-        }
-
-        /// <summary>
-        /// Lesson Title
-        /// </summary>
-        public static string Title
-        {
-            get
-            {
-                return "HeroExample: Simple animation";
-            }
-        }
-
-        #region IDisposable Members
-
-        private bool disposed;
-
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="disposing"></param>
-        protected virtual void Dispose(bool disposing)
-        {
-            if (!this.disposed)
-            {
-                if (disposing)
-                {
-                    for(int i= 0;i<characters.GetLength(0);i++)
-                    {
-                        if (characters[i] != null)
-                        {
-                            characters[i].Dispose();
-                            characters[i] = null;
-                        }
-                    }
-                }
-                this.disposed = true;
-            }
-        }
-        /// <summary>
-        /// 
-        /// </summary>
-        public void Dispose()
-        {
-            this.Dispose(true);
-            GC.SuppressFinalize(this);
-        }
-
-        /// <summary>
-        /// 
-        /// </summary>
-        public void Close()
-        {
-            Dispose();
-        }
-
-        /// <summary>
-        /// 
-        /// </summary>
-        ~FilsDeBerger()
-        {
-            Dispose(false);
-        }
-
-        #endregion
+        #endregion Methods
     }
 }
--- src/FilsDeBerger/SDL/Character.cs
+++ src/FilsDeBerger/SDL/Character.cs
@@ -1,75 +1,140 @@
-﻿using System;
-using System.IO;
-using System.Drawing;
-
-using SdlDotNet;
-using SdlDotNet.Graphics;
-using SdlDotNet.Graphics.Sprites;
-using SdlDotNet.Core;
-using SdlDotNet.Input;
-
-namespace FilsDeBerger.SDL
+﻿namespace FilsDeBerger.SDL
 {
+    using System.Drawing;
+
+    using SdlDotNet.Graphics;
+    using SdlDotNet.Graphics.Sprites;
+
+    #region Enumerations
+
+    /// <summary>
+    /// Indicates type of controller used for the character
+    /// </summary>
+    public enum Controller
+    {
+        /// <summary>
+        /// Player controlled sprite
+        /// </summary>
+        Player,
+
+        /// <summary>
+        /// Alternate player sprite
+        /// </summary>
+        AltPlayer,
+
+        /// <summary>
+        /// IA controlled sprite
+        /// </summary>
+        IA,
+
+        /// <summary>
+        /// Not moving sprite
+        /// </summary>
+        None
+    }
+
+    #endregion Enumerations
+
+    /// <summary>
+    /// Class used to draw and control characters
+    /// </summary>
     public class Character : AnimatedSprite
     {
+        #region Fields
+
+        /// <summary>
+        /// FileName of the sprite file
+        /// </summary>
+        private string fileName;
+
+        #endregion Fields
+
+        #region Constructors
+
+        /// <summary>
+        /// Initializes a new instance of the Character class.
+        /// </summary>
+        /// <param name="fileName">FileName of the sprite to load for the character</param>
+        public Character(string fileName)
+        {
+            this.FileName = fileName;
+        }
+
+        #endregion Constructors
+
+        #region Properties
+
+        /// <summary>
+        /// Gets or sets path of sprites used for characters
+        /// </summary>
         public static string Path
         {
             get;
             set;
         }
 
+        /// <summary>
+        /// Gets or sets FileName of the sprite file
+        /// </summary>
         public string FileName
         {
-            get;
-            set;
+            get
+            {
+                return this.fileName;
+            }
+
+            set
+            {
+                this.fileName = value;
+                string file = System.IO.Path.Combine(Character.Path, this.FileName);
+                Surface image = new Surface(file);
+
+                int animHeight = image.Height / 4;
+                int animWidth = image.Width / 4;
+
+                // Create the animation frames
+                SurfaceCollection walkDown = new SurfaceCollection();
+                walkDown.Add(image, new Size(animWidth, animHeight), 0);
+                SurfaceCollection walkLeft = new SurfaceCollection();
+                walkLeft.Add(image, new Size(animWidth, animHeight), 1);
+                SurfaceCollection walkRight = new SurfaceCollection();
+                walkRight.Add(image, new Size(animWidth, animHeight), 2);
+                SurfaceCollection walkUp = new SurfaceCollection();
+                walkUp.Add(image, new Size(animWidth, animHeight), 3);
+
+                // Add the animations to the hero
+                AnimationCollection animWalkUp = new AnimationCollection();
+                animWalkUp.Add(walkUp, animHeight - 1);
+                this.Animations.Add("WalkUp", animWalkUp);
+                AnimationCollection animWalkRight = new AnimationCollection();
+                animWalkRight.Add(walkRight, animHeight - 1);
+                this.Animations.Add("WalkRight", animWalkRight);
+                AnimationCollection animWalkDown = new AnimationCollection();
+                animWalkDown.Add(walkDown, animHeight - 1);
+                this.Animations.Add("WalkDown", animWalkDown);
+                AnimationCollection animWalkLeft = new AnimationCollection();
+                animWalkLeft.Add(walkLeft, animHeight - 1);
+                this.Animations.Add("WalkLeft", animWalkLeft);
+
+                // Change the transparent color of the sprite
+                Color animTrans = image.GetPixel(new Point(1, 1));
+                this.TransparentColor = animTrans;
+                this.Transparent = true;
+
+                // Setup the startup animation and make him not walk
+                this.CurrentAnimation = "WalkDown";
+            }
         }
 
+        /// <summary>
+        /// Gets or sets moving speed of the character
+        /// </summary>
         public int Speed
         {
             get;
             set;
         }
 
-        public Character(string fileName)
-        {
-            this.FileName = fileName;
-            string file = System.IO.Path.Combine(Character.Path, this.FileName);
-            Surface image = new Surface(file);
-
-            int animHeight = image.Height / 4;
-            int animWidth = image.Width / 4;
-
-            // Create the animation frames
-            SurfaceCollection walkDown = new SurfaceCollection();
-            walkDown.Add(image, new Size(animWidth, animHeight), 0);
-            SurfaceCollection walkLeft = new SurfaceCollection();
-            walkLeft.Add(image, new Size(animWidth, animHeight), 1);
-            SurfaceCollection walkRight = new SurfaceCollection();
-            walkRight.Add(image, new Size(animWidth, animHeight), 2);
-            SurfaceCollection walkUp = new SurfaceCollection();
-            walkUp.Add(image, new Size(animWidth, animHeight), 3);
-
-            // Add the animations to the hero
-            AnimationCollection animWalkUp = new AnimationCollection();
-            animWalkUp.Add(walkUp, animHeight - 1);
-            this.Animations.Add("WalkUp", animWalkUp);
-            AnimationCollection animWalkRight = new AnimationCollection();
-            animWalkRight.Add(walkRight, animHeight - 1);
-            this.Animations.Add("WalkRight", animWalkRight);
-            AnimationCollection animWalkDown = new AnimationCollection();
-            animWalkDown.Add(walkDown, animHeight - 1);
-            this.Animations.Add("WalkDown", animWalkDown);
-            AnimationCollection animWalkLeft = new AnimationCollection();
-            animWalkLeft.Add(walkLeft, animHeight - 1);
-            this.Animations.Add("WalkLeft", animWalkLeft);
-
-            // Change the transparent color of the sprite
-            Color animTrans = image.GetPixel(new Point(1, 1));
-            this.TransparentColor = animTrans;
-            this.Transparent = true;
-
-            // Setup the startup animation and make him not walk
-            this.CurrentAnimation = "WalkDown";
-        }
+        #endregion Properties
     }
 }
