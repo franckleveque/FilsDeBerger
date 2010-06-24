eb08447dac432b52dbdf28ea77ccfb296866f47d
Franck LEVEQUE <franck.leveque2@free.fr>
2010-06-24 22:41:15 +0200

    * Added background for VictoryArea

--- src/FilsDeBerger/SDL/StaticObjects/VictoryArea.cs
+++ src/FilsDeBerger/SDL/StaticObjects/VictoryArea.cs
@@ -20,7 +20,59 @@
         {
             this.UpperLeft = new Point(area.Left, area.Top);
             this.Area = area;
-            this.Fill(Color.Green);
+            // this.Fill(Color.Green);
+            Surface toFill = new Surface(new Size(32, 32));
+            int x = 0;
+            int y = 0;
+            // Get left upper corner
+            TileManager.FromTilemap(toFill, "003-G_Ground01.png", 0 * toFill.Width, 1 * toFill.Height);
+            this.Blit(toFill);
+            
+            // Upper separation of victory zone
+            TileManager.FromTilemap(toFill, "003-G_Ground01.png", 1 * toFill.Width, 1 * toFill.Height);
+            x += toFill.Width;
+            while (x < this.Width)
+            {
+                this.Blit(toFill, new Point(x, 0));
+                x += toFill.Width;
+            }
+
+            // Left separation of victory zone
+            TileManager.FromTilemap(toFill, "003-G_Ground01.png", 0 * toFill.Width, 2 * toFill.Height);
+            y += toFill.Height;
+            while (y < this.Height)
+            {
+                this.Blit(toFill, new Point(0, y));
+                y += toFill.Height;                
+            }
+
+            // Center of the victory zone
+            TileManager.FromTilemap(toFill, "003-G_Ground01.png", 2 * toFill.Width, 2 * toFill.Height);
+            x = toFill.Width;
+            y = toFill.Height;
+            while (x < this.Width)
+            {
+                while (y < this.Height)
+                {
+                    this.Blit(toFill, new Point(x, y));
+                    y += toFill.Height;
+                }
+
+                x += toFill.Width;
+            }
+
+            // Get left lower corner
+            TileManager.FromTilemap(toFill, "003-G_Ground01.png", 0 * toFill.Width, 3 * toFill.Height);
+            this.Blit(toFill, new Point(0, this.Height - toFill.Height));
+
+            // Lower separation of victory zone
+            TileManager.FromTilemap(toFill, "003-G_Ground01.png", 1 * toFill.Width, 3 * toFill.Height);
+            x += toFill.Width;
+            while (x < this.Width)
+            {
+                this.Blit(toFill, new Point(x, this.Height - toFill.Height));
+                x += toFill.Width;
+            }
         }
 
         #endregion Constructors
